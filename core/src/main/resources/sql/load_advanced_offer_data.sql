aabb75733aaa63d8404aa7f52f9d8becd8637990
Priyesh Patel <ppatel@broadleafcommerce.org>
2013-01-14 01:51:28 -0600

    using jpa transformer

---
+++ core/src/main/resources/sql/load_advanced_offer_data.sql
@@ -0,0 +1,10 @@
+--
+-- The Archetype is configured with "hibernate.hbm2ddl.auto" value="create-drop" in "persistence.xml".
+--
+-- This will cause hibernate to populate the database when the application is started by processing the files that
+-- were configured in the hibernate.hbm2ddl.import_files property.
+--
+-- This file is responsible for loading the the AdvancedOffer data used in the Archetype.   Implementers can change this file
+-- to load their initial catalog.
+--
+
